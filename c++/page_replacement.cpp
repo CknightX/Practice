6025401c2e2a2969f045ad6b2ac1288320e5acb8
CknightX <caokeck123@163.com>
2017-06-14 21:51:53 +0800

    2017.6.14

--- c++/page_replacement.cpp
+++ c++/page_replacement.cpp
@@ -2,36 +2,47 @@
 
 #include<iostream>
 #include<algorithm>
+#include<map>
+#include<vector>
+
 using namespace std;
 class Base
 {
 public:
 	Base(int n) :suspend_counter(0), block(new int[n]), block_size(n){ fill_n(block, block_size, 0); }
-	virtual void get(int page) = 0;
-	int get_suspend(){ return suspend_counter; }
-	void print_block(){ for_each(block, block + block_size, [](const int& i){if (i)cout << i << ' '; }); }
+	virtual void get(int* _page,size_t n) = 0;
+	int get_suspend(){ return suspend_counter; } //获取中断次数
+	int* is_in_block(int page){ auto result = find(block, block + block_size, page); return (result == block + block_size ? nullptr : result); } //是否位于页表
+	void move_page(int* page){ copy(page + 1, block + block_size, page); }  //移走某个页
+	void add_page(int page){ block[block_size - 1] = page; } //向页表末尾添加
+	void print_block(){ for_each(block, block + block_size, [](const int& i){if (i)cout << i << ' '; }); cout << endl; } //输出当前页表
 	~Base(){ delete[]block; }
 protected:
 	int suspend_counter;
 	int* block;
-	int block_size;
+	size_t block_size;
 };
 
 class FIFO :public Base
 {
 public:
 	FIFO(int n) :Base(n){}
-	void get(int page)
+	void get(int* _page,size_t n)
 	{
-		auto _end = block + block_size;
-		auto result = find(block, _end, page);
-		if (result != _end)
-			return;
-		else
+		for (size_t i = 0; i < n; ++i)
 		{
-			++suspend_counter; //ȱҳ
-			copy(block+1, _end, block);
-			block[block_size - 1] = page;
+			auto page = _page[i];
+			auto _end = block + block_size;
+			auto result = find(block, _end, page);
+			if (result != _end)
+				continue;
+			else
+			{
+				++suspend_counter;
+				move_page(block);
+				add_page(page);
+			}
+		print_block();
 		}
 	}
 };
@@ -40,34 +51,73 @@ class LRU :public Base
 {
 public:
 	LRU(int n) :Base(n){}
-	void get(int page)
+	void get(int* _page, size_t n)
 	{
-		auto _end = block + block_size;
-		auto result = find(block, _end, page);
-		if (result != _end)
+		for (size_t i = 0; i < n; ++i)
 		{
-			copy(result + 1, _end, result);
-			block[block_size - 1] = page;
+			auto page = _page[i];
+			auto _end = block + block_size;
+			auto result = find(block, _end, page);
+			if (result != _end)
+			{
+				move_page(result);
+			}
+			else
+			{
+				++suspend_counter;
+				move_page(block);
+			}
+			add_page(page);
+		print_block();
 		}
-		else
+	}
+};
+
+class OPT :public Base
+{
+public:
+	OPT(int n) :Base(n){}
+	void get(int* _page,size_t n)
+	{
+		map<int, int> page_num;
+		for (size_t i = 0; i < n; ++i)
 		{
-			++suspend_counter; //ȱҳ
-			copy(block + 1, _end, block);
-			block[block_size - 1] = page;
+			if (is_in_block(_page[i]))
+				continue;
+			else
+			{
+				++suspend_counter;
+				vector<int> result;
+				//  最久未使用
+				for (size_t j = 0; j < block_size; ++j) 
+				{
+					int len = 0;
+					for (size_t k = i + 1; k < n; ++k)
+					{
+						if (block[j] == _page[k])
+							break;
+						++len;
+					}
+					result.emplace_back(len);
+				}
+				size_t index = max_element(result.begin(), result.end()) - result.begin();
+				move_page(block + index);
+				add_page(_page[i]);
+			}
+		print_block();
 		}
 	}
 };
 int main()
 {
-	LRU test(3);
+	FIFO fifo(3);
+	LRU lru(3);
+	OPT opt(3);
 	int a[12] = { 2, 3, 2, 1, 5, 2, 4, 5, 3, 2, 5, 2 };
-	for (int i = 0; i < 12; ++i)
-	{
-		test.get(a[i]);
-		test.print_block();
-		cout << endl;
-	}
-	cout << test.get_suspend();
+	fifo.get(a, 12);
+	lru.get(a, 12);
+	opt.get(a, 12);
+	cout << fifo.get_suspend()<<endl<<lru.get_suspend()<<endl<<opt.get_suspend();
 	cin.get();
 	return 0;
 }
